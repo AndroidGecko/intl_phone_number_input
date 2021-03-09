8f12c2c38b1335e09de60d7252f1249e6943418b
Marcin Czech <contact@androidgecko.com>
2021-03-09 12:09:59 +0100

    Update intl_phone_number_input.dart

--- lib/intl_phone_number_input.dart
+++ lib/intl_phone_number_input.dart
@@ -1,5 +1,6 @@
 library intl_phone_number_input;
 
+export 'src/providers/country_provider.dart';
 export 'src/utils/phone_number.dart';
 export 'src/widgets/input_widget.dart';
 export 'src/utils/selector_config.dart';
